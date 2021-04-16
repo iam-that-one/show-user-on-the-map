66d21debb264b086a19fb8259dae8470edbdb76b
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-17 01:20:03 +0300

    Annotation added(PIN)

--- UserOnTheMap/Map.swift
+++ UserOnTheMap/Map.swift
@@ -11,7 +11,8 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     var coordinate: CLLocationCoordinate2D
-
+    let annotation = MKPointAnnotation()
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
@@ -19,7 +20,10 @@ struct MapView: UIViewRepresentable {
     func updateUIView(_ uiView: MKMapView, context: Context) {
         let span = MKCoordinateSpan(latitudeDelta: 100, longitudeDelta: 100)
         let region = MKCoordinateRegion(center: coordinate, span: span)
+        annotation.coordinate = coordinate
         uiView.setRegion(region, animated: true)
+        uiView.addAnnotation(annotation)
+            
     }
 }
 
