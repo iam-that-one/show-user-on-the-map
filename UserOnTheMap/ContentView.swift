5408065385d47d031e77c5f3a23ecd124aa65e73
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-17 00:29:11 +0300

    Initial Commit

---
+++ UserOnTheMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserOnTheMap
+//
+//  Created by Abdullah Alnutayfi on 17/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserOnTheMap/UserOnTheMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserOnTheMapApp.swift
+//  UserOnTheMap
+//
+//  Created by Abdullah Alnutayfi on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct UserOnTheMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
